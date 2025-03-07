1fb108b5e7e4fe781f75e88c99e6421803c53c10
Daniel S <von.sancho@gmail.com>
2025-03-07 07:06:46 +0100

    Actualizado a 07 de marzo 2025

---
+++ frontend/dist/assets/ListaPartida-BkLYQqCE.js
@@ -0,0 +1,1 @@
+import{_ as a,c as e,o as t}from"./index-BJuKcBFF.js";const c={};function r(s,n){return t(),e("h1",null,"Esta es la lista de partida")}const _=a(c,[["render",r]]);export{_ as default};
---
+++ frontend/dist/assets/NotFound-x6Uk0p5T.js
@@ -0,0 +1,1 @@
+import{_ as o,c as e,o as r}from"./index-BJuKcBFF.js";const c={};function n(t,a){return r(),e("h1",null," Error 404 : Page Not Found ")}const _=o(c,[["render",n]]);export{_ as default};
